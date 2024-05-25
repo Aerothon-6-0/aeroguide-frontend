5ebb10f15a0ab3693c8b7b588222f914765baaa9
Sunny Raj <94435695+rajnsunny@users.noreply.github.com>
2024-05-25 12:24:58 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import moment from 'moment-timezone'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function time(input : Date){
+  const utcTime = moment.utc(input);
+
+// Convert to Indian Standard Time (IST)
+const istTime = utcTime.tz('Asia/Kolkata');
+
+// Get the time in IST
+const istTimeString = istTime.format('HH:mm');
+
+return istTimeString;
+}
+
+export function date(input : Date){
+  const utcTime = moment.utc(input);
+
+// Convert to Indian Standard Time (IST)
+const istTime = utcTime.tz('Asia/Kolkata');
+
+// Get the time in IST
+const istDateString = istTime.format('MMM-DD');
+
+
+return istDateString;
 }
